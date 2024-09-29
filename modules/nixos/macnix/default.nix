e1bd976e66a05408e05ba75f526dfbb3f1658eed
Alexander Niedner <acniedner@gmail.com>
2024-09-29 14:33:26 +0200

    make sure nix uses local connection

--- modules/nixos/macnix/default.nix
+++ modules/nixos/macnix/default.nix
@@ -11,20 +11,21 @@
   virtualisation.docker.enable = true;
 
   environment.systemPackages = [
+    pkgs.cifs-utils
     pkgs.docker-compose # for docker projects
   ];
 
   # mount synologynas
   fileSystems = {
     "/mnt/entertainment" = {
-      device = "//synologynas/entertainment";
+      device = "//synologynas.fritz.box/entertainment";
       fsType = "cifs";
       options =
         let
           # this line prevents hanging on network split
-          automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000";
+          automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
         in
-        [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+        [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000" ];
     };
   };
 }
