89d5fd631b46242763be3ca9be145c781599999d
Alexander Niedner <acniedner@gmail.com>
2024-10-24 01:22:23 +0200

    Revert "switch synology to nfs"
    This reverts commit 4d12448903d0ff857da0a1b3cd411741482bff34.

--- modules/nixos/macnix/default.nix
+++ modules/nixos/macnix/default.nix
@@ -18,14 +18,14 @@
   # mount synologynas
   fileSystems = {
     "/mnt/entertainment" = {
-      device = "synologynas.fritz.box:/volume1/entertainment";
-      fsType = "nfs";
-      options = [
-        "nfsvers=4.1"
-        "x-systemd.automount"
-        "noauto"
-        "x-systemd.idle-timeout=600"
-      ];
+      device = "//synologynas.fritz.box/entertainment";
+      fsType = "cifs";
+      options =
+        let
+          # this line prevents hanging on network split
+          automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in
+        [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000" ];
     };
   };
 
