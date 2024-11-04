37f934d0c7644ebaf04e832bf77ba4dd9bd2541d
Alexander Niedner <acniedner@gmail.com>
2024-11-05 00:51:49 +0100

    update nixpkgs and set up basic hyprland config

--- modules/home-manager/nixos.nix
+++ modules/home-manager/nixos.nix
@@ -34,4 +34,37 @@
       };
     };
   };
+
+  wayland.windowManager.hyprland =
+    let
+      mainMod = "SUPER";
+    in
+    {
+      enable = true;
+      settings = {
+        bind =
+          [
+            "${mainMod}, F, exec, ${pkgs.firefox}/bin/firefox"
+            "${mainMod}, X, exec, ${pkgs.kitty}/bin/kitty"
+            "${mainMod}, C, killactive"
+            "${mainMod}, Q, exit"
+          ]
+          ++ (
+            # workspaces
+            # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+            builtins.concatLists (
+              builtins.genList (
+                i:
+                let
+                  ws = i + 1;
+                in
+                [
+                  "${mainMod}, code:1${toString i}, workspace, ${toString ws}"
+                  "${mainMod} SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                ]
+              ) 9
+            )
+          );
+      };
+    };
 }
