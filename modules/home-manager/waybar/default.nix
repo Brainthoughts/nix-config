c6633c34c81f17d586783046bc2b9d841f2c59c8
Alexander Niedner <acniedner@gmail.com>
2024-11-08 01:09:31 +0100

    initial waybar setup

--- modules/home-manager/waybar/default.nix
+++ modules/home-manager/waybar/default.nix
@@ -5,13 +5,40 @@
   ...
 }:
 
+let
+
+  update_interval = 5;
+  colors = {
+    white = "#f8f8f2";
+    black = "#ffffff";
+
+    red = "#dd532e";
+    orange = "#ff9977";
+    yellow = "#ffffa5";
+    green = "#69ff94";
+    blue = "#2aa9ff";
+  };
+
+  common_icon_colorscheme = icon: [
+    "<span color='${colors.green}'>${icon}</span>"
+    "<span color='${colors.blue}'>${icon}</span>"
+    "<span color='${colors.white}'>${icon}</span>"
+    "<span color='${colors.white}'>${icon}</span>"
+    "<span color='${colors.yellow}'>${icon}</span>"
+    "<span color='${colors.yellow}'>${icon}</span>"
+    "<span color='${colors.orange}'>${icon}</span>"
+    "<span color='${colors.red}'>${icon}</span>"
+  ];
+
+  icons = builtins.fromJSON (builtins.readFile ./icons.json);
+in
 {
   programs.waybar = {
     enable = true;
     settings = {
       mainBar = {
-        height = 30;
-        spacing = 4;
+        # height = 30;
+        spacing = 0; # deal with this in css
 
         modules-left = [
           "hyprland/workspaces"
@@ -23,28 +50,47 @@
 
         modules-right = [
           "pulseaudio"
+          # "bluetooth"
           "network"
-          "cpu"
           "memory"
-          "temperature"
-          "battery"
+          "cpu"
           "clock"
-          "tray"
         ];
 
-        tray = {
-          spacing = 10;
+        pulseaudio = {
+          scroll-step = 2.5;
+          format = "{volume}% ${icons.volume_high}";
         };
 
-        clock = {
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-          format-alt = "{:%Y-%m-%d}";
+        bluetooth = {
+
+        };
+
+        network = {
+          interval = update_interval;
+          format-ethernet = "{bandwidthUpBytes} ${icons.arrow_up_left}${icons.arrow_down_right} {bandwidthDownBytes}";
+          format-wifi = "{essid} ({signalStrength}%) ${icons.wifi}";
+          format-disconnected = "<span color='${colors.yellow}'>${icons.disconnected}</span>";
+          tooltip-format = "{ifname} : {ipaddr} : {cidr}";
+        };
+
+        memory = {
+          interval = update_interval;
+          format = "{percentage}% {icon}";
+          format-icons = common_icon_colorscheme icons.ram;
         };
 
         cpu = {
-          format = "{usage}% ";
-          tooltip = false;
+          interval = update_interval;
+          format = "{usage}% {icon}";
+          format-icons = common_icon_colorscheme icons.cpu;
         };
+
+        clock = {
+          interval = update_interval;
+          format = "{:%H:%M}";
+        };
+
       };
 
     };
