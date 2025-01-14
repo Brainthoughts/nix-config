963c31c8a6a715ef28c9346797f645263f6b03db
Alexander Niedner <acniedner@gmail.com>
2025-01-14 10:28:32 +0100

    configure oil

--- modules/home-manager/nvim/lua/plugins/ui/oil.lua
+++ modules/home-manager/nvim/lua/plugins/ui/oil.lua
@@ -1,3 +1,18 @@
 return {
 	"stevearc/oil.nvim",
+	opts = {
+		keymaps = {
+			["<C-s>"] = false,
+			["q"] = { "actions.close", mode = "n" },
+		},
+	},
+	keys = {
+		{
+			"<leader>o",
+			function()
+				require("oil").open_float()
+			end,
+			desc = "Open oil.nvim",
+		},
+	},
 }
