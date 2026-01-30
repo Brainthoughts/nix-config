44d7ec3eaa206d073d31412445c0b166a7a9e7ef
Alexander Niedner <acniedner@gmail.com>
2026-01-30 01:57:15 +0100

    setup regreet

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -43,6 +43,27 @@
   };
   programs.hyprlock.enable = true;
 
+  programs.regreet = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+    };
+    iconTheme = {
+      name = "Adwaita-dark";
+    };
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+    };
+    font = {
+      name = "Hack Nerd Font Mono";
+      package = pkgs.nerd-fonts.hack;
+    };
+    settings = {
+      application_prefer_dark_theme = true;
+    };
+  };
+
   # Configure console keymap
   console.keyMap = "us";
 
