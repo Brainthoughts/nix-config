6417b78045c011a41591415cbd997f1d2a0acd28
Alexander Niedner <acniedner@gmail.com>
2026-02-02 21:13:40 +0100

    fix macnix ssh

--- modules/home-manager/nixos/macnix/default.nix
+++ modules/home-manager/nixos/macnix/default.nix
@@ -1,8 +1,20 @@
 { pkgs, ... }:
 {
-
   imports = [
     ../default.nix
     # ./hyprland
   ];
+
+  programs.ssh.matchBlocks = {
+    "github.com" = {
+      identityFile = "~/.ssh/gh";
+    };
+    "*.tum.de" = {
+      identityFile = "~/.ssh/tum";
+    };
+    "gitlab.lrz.de" = {
+      identityFile = "~/.ssh/gh";
+    };
+  };
+
 }
