5410c25e79d411d0e8053c5c97ccecb4d32186e6
Alexander Niedner <acniedner@gmail.com>
2026-03-10 23:22:10 +0100

    add mini-surround

--- modules/home-manager/nvim/init.lua
+++ modules/home-manager/nvim/init.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
 		-- LazyVim Extras
 		{ import = "lazyvim.plugins.extras.dap.core" },
 
+		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
