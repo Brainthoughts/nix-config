ae393d79ad04cf86753083a08bcac2c2d2b43403
Alexander Niedner <acniedner@gmail.com>
2026-04-19 18:03:16 +0200

    init with flake-parts

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,73 +29,85 @@
   outputs =
     inputs@{
       self,
-      nix-darwin,
       nixpkgs,
+      flake-parts,
+      nix-darwin,
       home-manager,
       nix-index-database,
       nixos-apple-silicon,
     }:
-    let
-      hm-common = {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          verbose = true;
-          backupFileExtension = "bak";
-          sharedModules = [
-            nix-index-database.homeModules.nix-index
-          ];
-        };
-      };
-    in
-    {
-      darwinConfigurations = {
-        Sweet-16 = nix-darwin.lib.darwinSystem {
-          modules = [
-            ./modules/darwin
-            home-manager.darwinModules.home-manager
-            hm-common
-            {
-              home-manager.users.alexn = ./modules/home-manager/darwin.nix;
-            }
-          ];
-        };
-      };
-      nixosConfigurations = {
-        blacknix = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/nixos/blacknix
-            home-manager.nixosModules.home-manager
-            hm-common
-            {
-              home-manager.users.alexn = ./modules/home-manager/nixos/blacknix;
-            }
-          ];
-        };
-        macnix = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/nixos/macnix
-            home-manager.nixosModules.home-manager
-            hm-common
-            {
-              home-manager.users.alexn = ./modules/home-manager/nixos/macnix;
-            }
-          ];
-        };
-        pronix = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            nixos-apple-silicon.nixosModules.default
-            ./modules/nixos/pronix
-            home-manager.nixosModules.home-manager
-            hm-common
-            {
-              home-manager.users.alexn = ./modules/home-manager/nixos/pronix;
-            }
-          ];
-        };
-      };
-    };
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        flake =
+          let
+            hm-common = {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                verbose = true;
+                backupFileExtension = "bak";
+                sharedModules = [
+                  nix-index-database.homeModules.nix-index
+                ];
+              };
+            };
+          in
+          {
+            darwinConfigurations = {
+              Sweet-16 = nix-darwin.lib.darwinSystem {
+                modules = [
+                  ./modules/darwin
+                  home-manager.darwinModules.home-manager
+                  hm-common
+                  {
+                    home-manager.users.alexn = ./modules/home-manager/darwin.nix;
+                  }
+                ];
+              };
+            };
+            nixosConfigurations = {
+              blacknix = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [
+                  ./modules/nixos/blacknix
+                  home-manager.nixosModules.home-manager
+                  hm-common
+                  {
+                    home-manager.users.alexn = ./modules/home-manager/nixos/blacknix;
+                  }
+                ];
+              };
+              macnix = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [
+                  ./modules/nixos/macnix
+                  home-manager.nixosModules.home-manager
+                  hm-common
+                  {
+                    home-manager.users.alexn = ./modules/home-manager/nixos/macnix;
+                  }
+                ];
+              };
+              pronix = nixpkgs.lib.nixosSystem {
+                system = "aarch64-linux";
+                modules = [
+                  nixos-apple-silicon.nixosModules.default
+                  ./modules/nixos/pronix
+                  home-manager.nixosModules.home-manager
+                  hm-common
+                  {
+                    home-manager.users.alexn = ./modules/home-manager/nixos/pronix;
+                  }
+                ];
+              };
+            };
+          };
+      }
+    );
 }
