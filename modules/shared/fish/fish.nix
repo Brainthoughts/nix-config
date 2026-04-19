57ec91abc00e35c9378eba7caaae4871fe7923d5
Alexander Niedner <acniedner@gmail.com>
2026-04-19 20:21:18 +0200

    add fish homeModule

---
+++ modules/shared/fish/fish.nix
@@ -0,0 +1,23 @@
+{
+  flake.homeModules.fish =
+    { pkgs, ... }:
+    {
+      programs.fish = {
+        enable = true;
+        interactiveShellInit = builtins.readFile ./_/config.fish;
+        functions = {
+          fish_greeting = pkgs.lib.getExe pkgs.fastfetch;
+          fish_title = builtins.readFile ./_/fish_title.fish;
+        };
+        shellAliases = {
+          ff = "fastfetch";
+        };
+        shellAbbrs = {
+          nvnix = "nvim ~/.config/nix/**.nix";
+          norb = "nh os switch ~/.config/nix";
+          nort = "nh os test ~/.config/nix";
+          norr = "nh os rollback";
+        };
+      };
+    };
+}
