661610edfd23ef88ce1310f29ea734b4e5e3c826
Alexander Niedner <acniedner@gmail.com>
2026-05-20 23:06:09 +0200

    switch to 120 hz on pronix

--- modules/hosts/pronix/pronix.nix
+++ modules/hosts/pronix/pronix.nix
@@ -112,14 +112,14 @@ in
       };
 
       wayland.windowManager.hyprland.settings.monitor = [
-        "eDP-1, 3456x2160@60, 0x0, 1.5"
+        "eDP-1, 3456x2160@120, 0x0, 1.5"
       ];
 
       wayland.windowManager.hyprland.settings.bindl =
         # even when locked
         [
           ", switch:on:Apple SMC power/lid events, exec, hyprctl keyword monitor \"eDP-1, disable\""
-          ", switch:off:Apple SMC power/lid events, exec, hyprctl keyword monitor \"eDP-1, 3456x2160@60, 0x0, 1.5\""
+          ", switch:off:Apple SMC power/lid events, exec, hyprctl keyword monitor \"eDP-1, 3456x2160@120, 0x0, 1.5\""
         ];
     };
 }
