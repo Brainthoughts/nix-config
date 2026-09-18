48c73ab8b707b5a2dd7c50d63ab47756288db113
Alexander Niedner <acniedner@gmail.com>
2026-09-18 04:07:39 +0200

    disable the gcr ssh agent if 1password is enabled

--- modules/nixos/1password.nix
+++ modules/nixos/1password.nix
@@ -2,6 +2,7 @@
   flake.nixosModules."1password" =
     { config, ... }:
     {
+      services.gnome.gcr-ssh-agent.enable = false;
       programs = {
         _1password.enable = true;
         _1password-gui = {
